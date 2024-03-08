adf9fa1433d1d226ca08fd2dc615c2cd4d96a608
Olena Nesteruk <pavlena81@gmail.com>
2024-03-08 22:24:36 +0200

    added part task

---
+++ exercises/lecture-16/README.md
@@ -0,0 +1,1 @@
+Exercises for lecture №16 - JavaScript
--- exercises/lecture-16/assets/js/scripts.js
+++ exercises/lecture-16/assets/js/scripts.js
@@ -17,42 +17,42 @@ console.log(typeof numStr);
 
 //task 3
 
-let x = prompt("Введіть значення для x:");
-let y = prompt("Введіть значення для y:");
+// let x = prompt("Введіть значення для x:");
+// let y = prompt("Введіть значення для y:");
 
 // Перетворюємо введені значення у числа
-x = parseFloat(x);
-y = parseFloat(y);
+// x = parseFloat(x);
+// y = parseFloat(y);
 
 // Check if the numbers are correct
-if (isNaN(x) || isNaN(y)) {
-    console.log("Введені значення не є числами.");
-} else {
+// if (isNaN(x) || isNaN(y)) {
+//     console.log("Введені значення не є числами.");
+// } else {
     
-    let sum = x + y;
-    console.log("Sum x і y: " + sum);
-    alert(sum) 
-}
-//==========================================
-//or //Check if the numbers are correct
-if (!isNaN(x) && !isNaN(y)) {
+//     let sum = x + y;
+//     console.log("Sum x і y: " + sum);
+//     alert(sum) 
+// }
+// //==========================================
+// //or //Check if the numbers are correct
+// if (!isNaN(x) && !isNaN(y)) {
     
-    let sum = x + y;
-    console.log("Sum x і y: " + sum);
-} else {
-    console.log("The values you enter are not numbers.");
-}
+//     let sum = x + y;
+//     console.log("Sum x і y: " + sum);
+// } else {
+//     console.log("The values you enter are not numbers.");
+// }
 //========================================= 
 
 //task 4
 
-let a = 9007199254740991;
-let bigIntA=BigInt(a)
-let b = 9007199254740991n;
+// let a = 9007199254740991;
+// let bigIntA=BigInt(a)
+// let b = 9007199254740991n;
 
-let sum = bigIntA + b;
+// let sum = bigIntA + b;
 
-console.log(sum); 
+// console.log(sum); 
 
 //task 5
 
@@ -62,11 +62,47 @@ console.log(sum);
     //alert( apples + oranges ); 
 
 //task 6
+    'use strict'
+    // let a = 1, b = 1;
+    // let c = ++a; // c=2, після операції a стає 2
+    
+    // let d = b++; // d=1 d буде дорівнювати 1, оскільки b спочатку було 1, але після присвоєння b=2, оскільки змінна збільшилась на 1
    
+//7
+    let a = 2; // a *= 2 множить значення a на 2 і присвоює його назад до a. після виконання a *= 2, a буде мати значення 4
+    let x = 1 + (a *= 2);// x=5
+    
+//8
+'use strict';
 
+"" + 1 + 0 //10;
+   
+"" - 1 + 0//-1;
+  
+true + false// true or 1
+   
+6 / "3"//2;
 
-
-
-
-
-
+    
+"2" * "3"//6;
+    
+4 + 5 + "px"//9px;
+    
+"$" + 4 + 5//$45;
+  
+"4" - 2//2;
+   
+"4px" - 2//NaN;
+   
+"  -9  " + 5//-9 5 string;
+    console.log("  -9  " + 5);
+"  -9  " - 5//-14;
+   console.log("  -9  " - 5); 
+null + 1//1;
+    
+undefined + 1//NaN;
+   
+NaN + 1  //NaN;
+  
+    
+    
