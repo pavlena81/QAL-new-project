1c52190006d13a698368a6e1dd443abc1af86292
Olena Nesteruk <pavlena81@gmail.com>
2024-03-22 00:14:22 +0200

    added task 22 up to ex.seven

--- exercises/lecture-22/assets/js/app.js
+++ exercises/lecture-22/assets/js/app.js
@@ -27,4 +27,54 @@ for (const element of getAllElementByTeg) {
 const getElById = document.getElementById('p1');
 console.log(getElById);
 
-getElById.style.background='gold'
+getElById.style.background = 'gold';
+
+// task 3 ===
+// Отримати другий абзац за допомогою document.querySelector('#id') за його ідентифікатором p2.
+//  Встановити для нього стилі background - color: gold; color: blue; font - size: 2rem;
+
+const paragtaphTwo = document.querySelector('#p2');
+console.log(paragtaphTwo);
+paragtaphTwo.style.backgroundColor = 'gold';
+paragtaphTwo.style.color = 'blue';
+paragtaphTwo.style.fontSize = '2rem';
+
+// task 4 ====
+// Отримати третій абзац за допомогою document.querySelector('#id') за його ідентифікатором p3.
+//  Призначити для нього клас third
+
+const paragraphThree = document.querySelector('#p3');
+console.log(paragraphThree);
+paragraphThree.className = 'third';
+
+// task 5 ===
+// Отримати четвертий абзац за допомогою document.querySelector('#id')
+//  за його ідентифікатором p4.Призначити для нього класи fourth та border
+
+
+const paragraphFour = document.getElementById('p4');
+console.log(paragraphFour);
+paragraphFour.className = 'fourth';
+paragraphFour.classList.add('border'); 
+
+// task 6 ===
+// Знайти всі елементи з класом container. Використовуючи цикл for, 
+// вивести на консоль перший елемент для кожного зі знайдених елементів.
+
+const containers = document.getElementsByClassName('container');
+console.log(containers);
+
+
+for (const item of containers) {    
+    console.log(item.firstElementChild);
+};
+
+// task 7 ===
+// Знайти всі елементи з класом container. Використовуючи цикл for, вивести на консоль вміст першого елементу 
+// для кожного першого елемента зі знайдених елементів.
+
+for (const item of containers) {
+    console.log(item.firstElementChild.firstElementChild);
+}
+
+
