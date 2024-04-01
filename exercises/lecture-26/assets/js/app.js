19e5715da9bc9c3a2464ba3159a3e3512f4a2ea0
Olena Nesteruk <pavlena81@gmail.com>
2024-04-01 18:00:49 +0300

    added task 26 up to the end

--- exercises/lecture-26/assets/js/app.js
+++ exercises/lecture-26/assets/js/app.js
@@ -26,3 +26,51 @@ function calculate(callback, initialValue, numbers) {
 
 console.log(calculate(sum, 0, [1, 2, 4]));
 console.log(calculate(multiply, 1, [1, 2, 4]));
+
+// task 2 ===
+let student_names = ["Wick", "Malcolm", "Smith"];
+
+const studentNames =  student_names.map(function(item, index, array) {
+    console.log('name:',item,' index:', index, 'array:',array);
+});
+
+// task 3 ===
+
+const students_information = [
+    {"name": "Wick", "degree": 375}, 
+    {"name": "Malcolm", "degree": 405}, 
+    {"name": "Smith", "degree": 453},
+ ];
+
+ 
+const studentsInformation = students_information.map((item) => {
+
+    const persentage = ((item.degree*100)/600)
+     console.log('name:',item.name, ' degree:', item.degree, 'percentage:',persentage);
+})
+ 
+// task 4 === 
+
+const arr = [1, 2, 3, [10, 11, 12], 21, 22, 23, [31, 32, 33, 34], [41, 42]];
+
+const allArr = arr.reduce((acc, curr) => {
+    return acc.concat(curr) 
+}, []);
+console.log(allArr);
+
+// task 5 === 
+
+Array.prototype.upperCase = function () {
+let i;
+for (i = 0; i < this.length; i++) {
+this[i] = this[i].toUpperCase();
+}
+};
+
+function myFunc() {
+    let arr = ["Algorithm", "Data Structure", "Operating System", "html"];
+    arr.upperCase();
+    console.log(arr);
+}
+
+myFunc(); // [ 'ALGORITHM', 'DATA STRUCTURE', 'OPERATING SYSTEM', 'HTML' ]
