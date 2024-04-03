5f38b9c0020cfd975d89645393db33fb3fe3476e
Olena Nesteruk <pavlena81@gmail.com>
2024-04-03 16:29:11 +0300

    added task 27 up to the end

--- exercises/lecture-27/assets/js/app.js
+++ exercises/lecture-27/assets/js/app.js
@@ -1,10 +1,12 @@
 const ul = document.querySelector('ul');
 console.log(ul);
-const input = document.getElementById('item');
+
+// task === За допомогою JSON.parse прочитати елемент localStorage з ключем items та зберегти його в масиві itemsArray. 
+// Якщо елемент відсутній, створити його зі значенням[].
 
 const savedItems = localStorage.getItem('items');
 
-console.log(savedItems);
+// console.log(savedItems);
 
 const itemsArray = new Array();
 console.log(itemsArray);
@@ -15,6 +17,8 @@ console.log(parsedItems);
 itemsArray.push(parsedItems);
 console.log(itemsArray);
 
+
+// task === Написати функцію addTask(text)....
 
 function addTask(text) {
     const item = document.createElement('li'); 
@@ -28,6 +32,8 @@ function addTask(text) {
 
 addTask('to do home task');
 
+// task === Використовуючи метод forEach та функцію addTask...
+
 const createEl = ['create', 'read', 'update', 'delate'];
 console.log(createEl );
 
@@ -35,9 +41,42 @@ createEl.forEach(el=>{
     addTask(el)
 })
 
+// task === Написати функцію add()...
 
-function add() {
+const add = () => {
+
+    const input = document.getElementById('item');
+    const inputValue = input.value;
+    console.log(inputValue);
+
+    itemsArray.push(inputValue);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+
+    addTask(inputValue);
+
+    input.value = ' ';    
+    
 }
+
+const button = document.querySelector('button');
+button.addEventListener("click", add);
+
+// function add() {
+// }
+
+
+// task === Написати функцію del()...
 
 function del() {
+    localStorage.removeItem('items');
+    // console.log(localStorage.getItem('items'));
+    itemsArray.pop();
+
+    const ul = document.querySelector('ul');
+    const liToRemove = ul.lastElementChild;
+    
+    ul.removeChild(liToRemove);
+   
 }
+
+button.addEventListener("click", del);
