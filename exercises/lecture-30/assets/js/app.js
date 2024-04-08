5f056f4a823a1694ec9e1f7eb5a33b4106319900
Olena Nesteruk <pavlena81@gmail.com>
2024-04-09 01:40:43 +0300

    added task 30

---
+++ exercises/lecture-30/assets/js/app.js
@@ -0,0 +1,63 @@
+
+const template = (item) => `
+<h3>${item.title}</h3>
+<div>${item.body}</div>
+`;
+
+const form = document.getElementById('demoForm');
+console.log(form);
+const titleEl = document.getElementById('waiting');
+
+const handler = () => {
+       
+    const firstNameInput = form.querySelector('input[name="firstName"]');
+    const lastNameInput = form.querySelector('input[name="lastName"]');
+    
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+
+    console.log(firstName);
+    
+    if (firstName === "" && lastName === "") {
+        console.log("I'm miss You"); 
+    titleEl.textContent='I\'m miss You';
+    }else {
+        titleEl.textContent = `Hello ${firstName} ${lastName}!`;     }
+}
+// setInterval(handler, 10000);
+
+setTimeout(handler, 10000)
+
+form.addEventListener("input", handler);  
+
+
+// === 
+const demo = document.getElementById('demo');
+
+ const req = new XMLHttpRequest();
+    
+    req.responseType = 'json';
+    
+    req.open('GET', "https://jsonplaceholder.typicode.com/posts", true);
+    
+    
+req.onload = function () {
+    console.log(req.response);
+    handleResponse(req.response);    
+}
+  
+req.send();
+
+function handleResponse(response) {
+    
+     const markup = response
+    .map(({title, body}) => {
+      return `<li>
+          <h2>${title.slice(0, 30)}</h2>          
+          <p>${body}</p>
+        </li>`;
+    })
+    .join("");
+  demo.innerHTML = markup;
+}
+  
