4da442dc47fc04fb8a01b0af8fd0b089b0f5afe2
Olena Nesteruk <pavlena81@gmail.com>
2024-05-29 17:28:08 +0300

    update index & vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig({
+  base: '/QAL-new-project/',
   plugins: [
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
